cb5a21786635e0a513675b33625ea34a828ffde3
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-18 14:04:59 +0000

    fixed wrong import error

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add 
 if __name__ == "__main__":
     a, b = 1, 2
-    c = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, add(a, b))
