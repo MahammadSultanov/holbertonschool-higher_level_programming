557670919cbf7d08ebfaea15bf391f544fdad8f8
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-18 20:28:20 +0000

    pycode style fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if argc == 1:
         print("0")
     else:
-        for i in range (1, argc):
+        for i in range(1, argc):
             i = int(argv[i])
             c = c + i
         print("{}".format(c))
