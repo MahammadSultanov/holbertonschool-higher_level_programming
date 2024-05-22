65935d0d5f16b35fe1e70facaf08079a7a852747
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-22 11:05:02 +0000

    case: matrix = [[]] fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r in matrix:
-        for idx, j in enumerate(r):
-            if idx < len(r) - 1:
-                print("{:d}".format(j), end=' ')
-            else:
-                print("{:d}".format(j))
+    if matrix != [[]]:
+        for r in matrix:
+            for idx, j in enumerate(r):
+                if idx < len(r) - 1:
+                    print("{:d}".format(j), end=' ')
+                else:
+                    print("{:d}".format(j))
+    else:
+        print()
