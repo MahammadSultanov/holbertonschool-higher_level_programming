baf2945808b1fcddd59646dc37bab99714511e4c
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-24 09:27:49 +0000

    pycodestyle fixed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (0,None)
+        return (0, None)
     else:
-        l = (len(sentence), sentence[0])
-        return l
+        result = (len(sentence), sentence[0])
+        return result
