7f6a43f86a71d97b3313b8c7fbdbe459067fc241
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-25 19:36:42 +0000

    fixed case: You have to use try: / except: / finally:

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    res = None
     try:
         res = a / b
-        print("Inside result: {}".format(res))
         return res
     except (ZeroDivisionError):
-        print("Inside result: None")
         return None
+    finally:
+        print("Inside result: {}".format(res))
