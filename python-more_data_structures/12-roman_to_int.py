6ae09c30f7ef9e617d0c5226d60e3e5a26daa4a2
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-25 16:38:16 +0000

    fixed case: Correct output: not a string and Correct output: None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_string = roman_string.upper()
     roman_num = {
         "I": 1,
         "V": 5,
@@ -12,12 +11,14 @@ def roman_to_int(roman_string):
     }
     result = 0
     prev = 0
+    if roman_string is None or not isinstance(roman_string, str):
+        return result
+    roman_string = roman_string.upper()
     for symbol in reversed(roman_string):
         value = roman_num[symbol]
         if value >= prev:
             result += value
-            prev = value
         else:
             result -= value
-            prev = value
+        prev = value
     return result
