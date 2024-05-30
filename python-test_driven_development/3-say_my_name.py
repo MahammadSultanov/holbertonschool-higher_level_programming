0c086a2eec0ec865436b1f34670188a6ff030633
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-30 13:42:37 +0000

    I Won.

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,6 +10,6 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     if first_name is None:
-        raise TypeError("say_my_name() missing 1 required
-                        positional argument: 'first_name")
+        raise TypeError("say_my_name() missing 1 required"
+                        "positional argument: 'first_name")
     print("My name is {} {}".format(first_name, last_name))
