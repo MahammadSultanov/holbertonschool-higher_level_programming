78baf003e5f31f69c6df9ae64728a95e32b90c72
MuhammedSultanov <9073@holbertonstudents.com>
2024-05-31 16:10:58 +0400

    empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Unittests for max_integer([..])"""
+
+
+class Rectangle:
+    """This class about rectangle"""
+    pass
+
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
