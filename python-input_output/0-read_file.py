c141d0de49a09000dd1bd6426c697dacdce6d361
MuhammedSultanov <9073@holbertonstudents.com>
2024-06-04 21:21:00 +0400

    documantion fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module for Read file task
+"""
+
+
 def read_file(filename=""):
+    """This function read file from filename var"""
     with open(filename, 'r', encoding="UTF8") as file:
         print(file.read())
         file.close()
