0edfa1964e0a45e5ba6e2418a937932240c77822
MuhammedSultanov <9073@holbertonstudents.com>
2024-06-04 21:24:12 +0400

    some bugs fixed

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ This module for Read file task
 def read_file(filename=""):
     """This function read file from filename var"""
     with open(filename, 'r', encoding="UTF8") as file:
-        print(file.read())
+        print(file.read(), end="")
         file.close()
