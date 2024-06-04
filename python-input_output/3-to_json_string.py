7e3f35deec59fe9d14e15baacbca0802ac220552
MuhammedSultanov <9073@holbertonstudents.com>
2024-06-04 21:40:24 +0400

    JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+"""
+This module for To JSON string task
+"""
+
+
+def to_json_string(my_obj):
+    """This function that returns the JSON representation of an object  """
+    return json.dumps(my_obj)
