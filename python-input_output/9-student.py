950fef0c055448fb93ad399bf8250cf84d0d15c1
MuhammedSultanov <9073@holbertonstudents.com>
2024-06-05 12:33:31 +0400

    pycode fixed

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -12,5 +12,6 @@ class Student:
         self.age = age
 
     def to_json(self):
-        """This function retrieves a dictionary representation of a Student instance"""
+        """This function retrieves a dictionary
+         representation of a Student instance"""
         return self.__dict__
